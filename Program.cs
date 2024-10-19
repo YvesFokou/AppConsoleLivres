22f45a7afeda422c006c24f5cf4baba7152c629b
Yves Fokou <300138329@monboreal.ca>
2024-10-19 14:30:26 -0700

    Ajout du fichier Revue

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Revue revue1 = new Revue("Nation Negres et culture", "Cheihk anta Diop", 2020, 123, 2022);
+        Revue revue2 = new Revue("Goovi REhkmire", "histoires des cvilisations Negres", 2021, 456, 2022);
+        Revue revue3 = new Revue("civilisation ou barbarie", "Cheihk Anta Diop", 2019, 789, 2022);
+
+        List<Livre> livres = new List<Livre> { revue1, revue2, revue3 };
+        Console.WriteLine("les livres; ");
+        foreach (Livre l in livres)
+        {
+            l.AfficherDetails();
+        }
     }
 }
---
+++ Revue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    public class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+        public Revue(string titre, string auteur, int nbrPages, int numero, int annee)
+            : base(titre, auteur, nbrPages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Numéro: {Numero},Annee; {Annee}");
+        }
+    }
+}
