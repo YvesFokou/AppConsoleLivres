02d8993453120f93505f45c0574fd24cd53a2c6f
Yves Fokou <300138329@monboreal.ca>
2024-10-19 14:18:31 -0700

    Ajout des classes

---
+++ Livre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    public class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NbrPages { get; set; }
+
+        public Livre(string titre, string auteur, int nbrPages)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NbrPages = nbrPages;
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Année: {NbrPages}");
+        }
+    }
+
+}
