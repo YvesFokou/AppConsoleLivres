aef8ad78556125089bae6a050c6a7a0364658574
Yves Fokou <300138329@monboreal.ca>
2024-10-19 14:56:27 -0700

    ajout du fichier program.cs

--- Program.cs
+++ Program.cs
@@ -5,11 +5,18 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        Revue revue1 = new Revue("Nation Negres et culture", "Cheihk anta Diop", 2020, 123, 2022);
+        Revue revue1 = new Revue("Nation Negres et culture", "Cheihk anta Diop", 2021, 123, 1989);
         Revue revue2 = new Revue("Goovi REhkmire", "histoires des cvilisations Negres", 2021, 456, 2022);
-        Revue revue3 = new Revue("civilisation ou barbarie", "Cheihk Anta Diop", 2019, 789, 2022);
+        Revue revue3 = new Revue("civilisation ou barbarie", "Cheihk Anta Diop", 2019, 789, 1996);
 
-        List<Livre> livres = new List<Livre> { revue1, revue2, revue3 };
+        Roman roman1 = new Roman("L'enfant Noir", "Camara Laye", 197, "Aventure");
+        Roman roman2 = new Roman("Saison de la migration vers le Nord", "Tayeb Sahil", 271, "romanesque");
+        Roman roman3 = new Roman("Etonner les Dieux", "Ben Okri", 335, "Drame");
+
+
+
+        List<Livre> livres = new List<Livre> { revue1, revue2, revue3 , roman1, roman2, roman3 };
+
         Console.WriteLine("les livres; ");
         foreach (Livre l in livres)
         {
