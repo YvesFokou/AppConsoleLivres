d6b8ce8b3ad3b8a0e7a5c3793ce1214a44e3475f
Yves Fokou <300138329@monboreal.ca>
2024-10-19 14:37:28 -0700

    Ajout de la classe Roman.cs

---
+++ Roman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    public class Roman : Livre
+    {
+        public string Genre { get; set; }
+
+        public Roman(string titre, string auteur, int nbrPages, string genre)
+            : base(titre, auteur, nbrPages)
+        {
+            Genre = genre;
+        }
+
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+}
